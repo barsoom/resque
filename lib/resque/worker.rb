9242a08d30677a1e106e414aea9452b60ab63f6e
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-08-31 15:54:42 +0200

    More debug

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -371,8 +371,8 @@ debuginfo('RDEBUG: got exception' + exception.inspect)
     # USR2: Don't process any new jobs
     # CONT: Start processing jobs again after a USR2
     def register_signal_handlers
-      trap('TERM') { shutdown!  }
-      trap('INT')  { shutdown!  }
+      trap('TERM') { debuginfo("GOT TERM!?"); shutdown!  }
+      trap('INT')  { debuginfo("GOT INT!?"); shutdown!  }
 
       begin
         trap('QUIT') { debuginfo('RDEBUG: got QUIT');  shutdown   }
